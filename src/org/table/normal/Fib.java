2b48e89fa5a8a040901026685edb1fad46b5ce9a
tablesheep233 <858916094@qq.com>
2021-04-25 22:50:31 +0800

    Fibonacci add answer

--- src/org/table/normal/Fib.java
+++ src/org/table/normal/Fib.java
@@ -4,6 +4,7 @@ package org.table.normal;
  * Fibonacci
  */
 public class Fib {
+    //递归
 //    public int fib(int n) {
 //        if (n == 0) {
 //            return 0;
@@ -14,17 +15,32 @@ public class Fib {
 //        return (fib(n - 1) + fib(n - 2)) %= 1000000007;
 //    }
 
+    //动态规划
+//    public int fib(int n) {
+//        if(n == 0) {
+//            return 0;
+//        }
+//        int dp[] = new int[n + 1];
+//        dp[0] = 0;
+//        dp[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//            dp[i] %= 1000000007;
+//        }
+//        return dp[n];
+//    }
+
+    //交换变量
     public int fib(int n) {
-        if(n == 0) {
-            return 0;
+        if (n <= 1) {
+            return n;
         }
-        int dp[] = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
+        int x = 0, y = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-            dp[i] %= 1000000007;
+            int temp = (x + y) % 1000000007;
+            x = y;
+            y = temp;
         }
-        return dp[n];
+        return y;
     }
 }
