e6e2ef785db4e1c1aaabdcfc9b8cde35106141bd
tablesheep233 <858916094@qq.com>
2021-05-01 21:23:03 +0800

    leetcode 20
    20 括号字符串是否有效

---
+++ src/org/table/other/isValidBrackets_20.java
@@ -0,0 +1,40 @@
+package org.table.other;
+
+import java.util.*;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ */
+public class isValidBrackets_20 {
+    Map<Character, Character> map = new HashMap();
+    {
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+    }
+
+
+    public boolean isValid(String s) {
+        if ((s.length() & 1) == 1) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || !map.get(c).equals(stack.pop())) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
