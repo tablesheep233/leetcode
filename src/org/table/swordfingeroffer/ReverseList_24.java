97d0f865730e7784c93f8f2ece1ed4a89f12fcb1
tablesheep233 <858916094@qq.com>
2021-05-12 21:39:21 +0800

    sword finger offer 24
    剑指offer 24 反转链表

---
+++ src/org/table/swordfingeroffer/ReverseList_24.java
@@ -0,0 +1,23 @@
+package org.table.swordfingeroffer;
+
+/**
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ */
+public class ReverseList_24 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
