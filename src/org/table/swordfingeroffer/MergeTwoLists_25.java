bc0e13082b890e17bf4e9cd0afe78213a23c40b5
tablesheep233 <858916094@qq.com>
2021-05-19 21:03:05 +0800

    sword finger offer 25
    剑指offer 25 合并两个排序的链表

---
+++ src/org/table/swordfingeroffer/MergeTwoLists_25.java
@@ -0,0 +1,23 @@
+package org.table.swordfingeroffer;
+
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ */
+public class MergeTwoLists_25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode node = temp;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        node.next = l1 == null ? l2 : l1;
+        return temp.next;
+    }
+}
