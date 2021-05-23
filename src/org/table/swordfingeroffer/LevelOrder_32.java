c2e52f7bec0ad45638b7eb41a50ae23d76fd3050
tablesheep233 <858916094@qq.com>
2021-05-23 20:35:13 +0800

    sword finger offer 32-II
    剑指offer 32-II  从上到下打印二叉树 II

--- src/org/table/swordfingeroffer/LevelOrder_32.java
+++ src/org/table/swordfingeroffer/LevelOrder_32.java
@@ -5,12 +5,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
- */
+
 public class LevelOrder_32 {
 
-    public int[] levelOrder(TreeNode root) {
+    /**
+     * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+     */
+    public int[] levelOrder_I(TreeNode root) {
         if(root == null) {
             return null;
         }
@@ -27,6 +28,34 @@ public class LevelOrder_32 {
         int[] result = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    /**
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     */
+    public List<List<Integer>> levelOrder_II(TreeNode root) {
+        if(root == null) {
+            return new LinkedList<>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<List<Integer>> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int len = queue.size();
+            for(int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(list);
         }
         return result;
     }
