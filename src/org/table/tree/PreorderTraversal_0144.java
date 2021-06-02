390cbd6034f5d7cc2e73fbcdfb59cc6281b06b2e
tablesheep233 <858916094@qq.com>
2021-06-02 22:33:43 +0800

    leetcode 144
    144 二叉树的前序遍历

---
+++ src/org/table/tree/PreorderTraversal_0144.java
@@ -0,0 +1,24 @@
+package org.table.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ */
+public class PreorderTraversal_0144 {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorderTraversal(root, list);
+        return list;
+    }
+
+    private void preorderTraversal(TreeNode node, List<Integer> list) {
+        if (node != null) {
+            list.add(node.val);
+            preorderTraversal(node.left, list);
+            preorderTraversal(node.right, list);
+        }
+    }
+}
