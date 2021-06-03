52b23f9776f75c8f87f4162b1fe1ebb4af5a2c3e
tablesheep233 <858916094@qq.com>
2021-06-03 19:57:07 +0800

    leetcode 104
    104 二叉树的最大深度

---
+++ src/org/table/tree/MaxDepth_0104.java
@@ -0,0 +1,18 @@
+package org.table.tree;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ */
+public class MaxDepth_0104 {
+
+    public int maxDepth(TreeNode root) {
+        if(root != null) {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+        return 0;
+    }
+}
